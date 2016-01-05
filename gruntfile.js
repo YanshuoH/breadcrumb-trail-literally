e9146c11d8910faca091c751e8164ea88af1249b
Yanshuo HUANG <yanshuo.huang.ext@francetv.fr>
2016-01-05 14:47:07 +0000

    coverage grunt task

---
+++ gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkgFile: 'package.json',
+    mocha_istanbul: {
+      coverage: {
+        src: ['test/*Test.js'],
+        options: {
+          coverageFolder: 'reports/coverage',
+          reportFormats: [
+            'cobertura',
+            'html',
+          ],
+        },
+      },
+    },
+    istanbul_check_coverage: {
+      default: {
+        options: {
+          coverageFolder: 'reports/coverage',
+          check: {
+            lines: 80,
+            statements: 80,
+          },
+        },
+      },
+    },
+  });
+
+  grunt.registerTask('coverage', ['mocha_istanbul']);
+  grunt.registerTask('build', function() {
+    // do nothing
+  });
+}
